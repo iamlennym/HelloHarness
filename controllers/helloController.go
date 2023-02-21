66d32bb275f9f330a5f16bfdede35079cf1aff44
iamlennym <iamlennym@gmail.com>
2023-02-21 08:28:55 +0200

    Update helloController.go

--- controllers/helloController.go
+++ controllers/helloController.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-var keyword = "hello-1234-7"
+var keyword = "hello-1234-1"
 
 func SayHello(c *fiber.Ctx) error {
 	str := fmt.Sprintf("Hello from 'HELLO HARNESS API Service with keyword : [%s]'...", keyword)
